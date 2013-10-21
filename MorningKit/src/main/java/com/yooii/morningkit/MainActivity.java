ec7d84815a016dc4ab969b87b09d4f097bfda016
Wooseong Kim <kwosu87@me.com>
2013-10-21 18:07:27 +0900

    [Delivers #59203676] AppRate.jar 포함 및 테스트 완료.(액티비티 실행). ./gradlew clean 을 통해서 해결(chmod 777로 permission 도 변경해줌)

--- MorningKit/src/main/java/com/yooii/morningkit/MainActivity.java
+++ MorningKit/src/main/java/com/yooii/morningkit/MainActivity.java
@@ -1,15 +1,15 @@
 package com.yooii.morningkit;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import com.tjeannin.apprate.AppRate;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -23,6 +23,8 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        new AppRate(this).init();
     }
 
 
