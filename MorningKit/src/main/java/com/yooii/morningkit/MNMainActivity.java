403ee2a161b101cc9fe84bcfe9cb0ee787f983a4
Yongbin Bae <lux0208@naver.com>
2013-10-22 14:57:55 +0900

    작업중

---
+++ MorningKit/src/main/java/com/yooii/morningkit/MNMainActivity.java
@@ -0,0 +1,16 @@
+package com.yooii.morningkit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MNMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 주석처리
+        // 주석처리 하나 더 
+    }
+}
