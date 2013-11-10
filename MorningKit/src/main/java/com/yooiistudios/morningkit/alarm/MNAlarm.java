bec225135832078379001e61af05199405278d3d
Wooseong Kim <kwosu87@me.com>
2013-11-11 01:17:13 +0900

    알람 필수 모델들 틀을 잡아둠.

---
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/alarm/MNAlarm.java
@@ -0,0 +1,12 @@
+package com.yooiistudios.morningkit.alarm;
+
+/**
+ * Created by StevenKim on 2013. 11. 11..
+ */
+public class MNAlarm {
+    private static final String TAG = "MNAlarm";
+
+    public MNAlarm() {
+
+    }
+}
---
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/alarm/MNAlarmMaker.java
@@ -0,0 +1,12 @@
+package com.yooiistudios.morningkit.alarm;
+
+/**
+ * Created by StevenKim on 2013. 11. 11..
+ */
+public class MNAlarmMaker {
+    private static final String TAG = "MNAlarmMaker";
+
+    public static void makeAlarm() {
+
+    }
+}
