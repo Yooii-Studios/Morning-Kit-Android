1963aa11a1f2fa64c07e511b2b2336b3f06984c0
Wooseong Kim <kwosu87@me.com>
2013-11-11 01:21:21 +0900

    알람 테스트들도 틀 잡아둠

---
+++ MorningKit/src/test/java/com/yooiistudios/morningkit/alarm/MNAlarmMakerTest.java
@@ -0,0 +1,29 @@
+package com.yooiistudios.morningkit.alarm;
+
+import com.yooiistudios.morningkit.common.RobolectricGradleTestRunner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+//import static org.junit.matchers.JUnitMatchers.*;
+
+/**
+ * Created by StevenKim on 2013. 11. 11..
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+public class MNAlarmMakerTest {
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void makeNewAlarmListTest() {
+
+    }
+}
---
+++ MorningKit/src/test/java/com/yooiistudios/morningkit/alarm/MNAlarmTest.java
@@ -0,0 +1,30 @@
+package com.yooiistudios.morningkit.alarm;
+
+import com.yooiistudios.morningkit.common.RobolectricGradleTestRunner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+//import static org.junit.matchers.JUnitMatchers.*;
+
+/**
+ * Created by StevenKim on 2013. 11. 11..
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+public class MNAlarmTest {
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void alarmConstructorTest() {
+        MNAlarm alarm = new MNAlarm();
+        assertNotNull(alarm);
+    }
+}
