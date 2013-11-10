90ee061408d4113d2ac125b065a5edf8eb728b04
Wooseong Kim <kwosu87@me.com>
2013-11-11 01:23:12 +0900

    테스트 코드

--- MorningKit/src/test/java/com/yooiistudios/morningkit/alarm/MNAlarmListManagerTest.java
+++ MorningKit/src/test/java/com/yooiistudios/morningkit/alarm/MNAlarmListManagerTest.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 //import static org.junit.matchers.JUnitMatchers.*;
@@ -17,18 +19,23 @@ import static org.hamcrest.CoreMatchers.*;
  */
 @RunWith(RobolectricGradleTestRunner.class)
 public class MNAlarmListManagerTest {
+    ArrayList<MNAlarm> alarmListForTestSave;
+    ArrayList<MNAlarm> alarmListForTestLoad;
+
     @Before
     public void setUp() {
+        // 미리 save 테스트용 ArrayList, load 테스트용 ArrayList를 만들어 두자
+        alarmListForTestSave = new ArrayList<MNAlarm>();
+        alarmListForTestLoad = new ArrayList<MNAlarm>();
+    }
+
+    @Test
+    public void saveAlarmListTest() {
 
     }
 
     @Test
     public void loadAlarmListTest() {
-
-    }
-
-    @Test
-    public void saveAlarmListTest() {
 
     }
 }
