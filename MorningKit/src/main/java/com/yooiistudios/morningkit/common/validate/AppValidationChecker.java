59078ca61829b3340146276ed2d36efcae36a1fc
Wooseong Kim <kwosu87@me.com>
2013-11-11 00:41:38 +0900

    ValidateChecker 추가

---
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/common/validate/AppValidationChecker.java
@@ -0,0 +1,44 @@
+package com.yooiistudios.morningkit.common.validate;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+// 윤승용 제작. 김우성 감수 (특정 날짜를 지정해놓고 메인 액티비티의 onCreate에서 사용하면 날짜에 따라 알아서 죽음)
+public class AppValidationChecker
+{
+    private static final int VALIDATE_YEAR = 2013;
+    
+    private static final int VALIDATE_MONTH = Calendar.NOVEMBER;
+    
+    private static final int VALIDATE_DAY = 30;
+    
+    public static void validationCheck(Activity c)
+    {
+        Calendar calendarForCheck = Calendar.getInstance();
+        
+        boolean isValidate = true;
+        
+        if (calendarForCheck.get(Calendar.YEAR) >= VALIDATE_YEAR)
+        {
+            if (calendarForCheck.get(Calendar.MONTH) > VALIDATE_MONTH)
+            {
+                isValidate = false;
+            }
+            else if (calendarForCheck.get(Calendar.MONTH) == VALIDATE_MONTH)
+            {
+                if (calendarForCheck.get(Calendar.DAY_OF_MONTH) > VALIDATE_DAY)
+                {
+                    isValidate = false;
+                }
+            }
+        }
+        
+        if (!isValidate)
+        {
+            Toast.makeText(c.getApplicationContext(), "Morning Kit is on the validation date.", Toast.LENGTH_SHORT).show();
+            c.finish();
+        }
+    }
+}
