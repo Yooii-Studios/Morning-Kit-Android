861af972c7b67ddaaab929c8fa6e0cc68a406cc8
Wooseong Kim <kwosu87@me.com>
2013-11-25 19:22:26 +0900

    MNAlarmTest 작성 중

--- MorningKit/src/test/java/com/yooiistudios/morningkit/alarm/MNAlarmTest.java
+++ MorningKit/src/test/java/com/yooiistudios/morningkit/alarm/MNAlarmTest.java
@@ -8,6 +8,9 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 //import static org.junit.matchers.JUnitMatchers.*;
@@ -28,6 +31,17 @@ public class MNAlarmTest {
     @Test
     // 필요한 정보들이 null값이 아닌지 테스트
     public void alarmConstructorTest() {
-        assertNotNull(testAlarm);
+        assertThat(testAlarm, notNullValue());
+        assertThat(testAlarm.isAlarmOn, is(true));
+        assertThat(testAlarm.isSnoozeOn, is(false));
+        assertThat(testAlarm.isRepeatOn, is(false));
+        assertThat(testAlarm.alarmLabel, is("Alarm"));
+        assertThat(testAlarm.alarmID, is(not(-1)));
+        assertThat(testAlarm.alarmCalendar, notNullValue());
+
+        for (int i = 0; i < testAlarm.alarmRepeatOnOfWeek.size(); i++) {
+            Boolean alarmRepeatOnSpecificWeekday = testAlarm.alarmRepeatOnOfWeek.get(i);
+            assertThat(alarmRepeatOnSpecificWeekday, is(false));
+        }
     }
 }
