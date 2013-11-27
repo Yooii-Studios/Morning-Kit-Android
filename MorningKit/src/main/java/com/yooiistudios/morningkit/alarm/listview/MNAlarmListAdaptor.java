f7e8a2da974e83bc8392244c0f4ecc29f1001aa0
Wooseong Kim <kwosu87@me.com>
2013-11-27 21:29:56 +0900

    MNAlarmListAdaptor 틀잡기

---
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/alarm/listview/MNAlarmListAdaptor.java
@@ -0,0 +1,39 @@
+package com.yooiistudios.morningkit.alarm.listview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.yooiistudios.morningkit.alarm.model.MNAlarm;
+
+import java.util.ArrayList;
+
+/**
+ * Created by StevenKim in MorningKit from Yooii Studios Co., LTD. in ${PROJECT_NAME} from Yooii Studios Co., LTD. in ${PROJECT_NAME} from Yooii Studios Co., LTD. in ${PROJECT_NAME} from Yooii Studios Co., LTD. on 2013. 11. 27..
+ */
+public class MNAlarmListAdaptor extends BaseAdapter {
+    private static final String TAG = "MNAlarmListAdapter";
+    private ArrayList<MNAlarm> m_alarmList;
+    private LayoutInflater layoutInflater;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
