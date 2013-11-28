084aff38ceb1262c04f7864afb92f9038efe4592
Wooseong Kim <kwosu87@me.com>
2013-11-28 09:10:13 +0900

    MNAlarmIdMaker 싱글톤 취소, static 변수로 변경

--- MorningKit/src/main/java/com/yooiistudios/morningkit/alarm/model/MNAlarmIdMaker.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/alarm/model/MNAlarmIdMaker.java
@@ -11,23 +11,8 @@ import com.yooiistudios.morningkit.MN;
  */
 public class MNAlarmIdMaker {
 
-    /**
-     * Singleton
-     */
-    private volatile static MNAlarmIdMaker instance;
-    private volatile SharedPreferences prefs;
+    private static SharedPreferences prefs;
     private MNAlarmIdMaker() { throw new AssertionError(); } // You shouldn't create instance
-    public static MNAlarmIdMaker getInstance(Context context) {
-        if (instance == null) {
-            synchronized (MNAlarmIdMaker.class) {
-                if (instance == null) {
-                    instance = new MNAlarmIdMaker();
-                    instance.prefs = context.getSharedPreferences(MN.alarm.SHARED_PREFS_FILE, Context.MODE_PRIVATE);
-                }
-            }
-        }
-        return instance;
-    }
 
     /**
      * get validate Id automatically. increase 8 for each id.
@@ -36,21 +21,20 @@ public class MNAlarmIdMaker {
      */
     public static int getValidAlarmID(Context context) {
 
-        int alarmId = -1;
-        SharedPreferences prefs = MNAlarmIdMaker.getInstance(context).prefs;
-        if (prefs != null) {
-            alarmId = prefs.getInt("alarmId", -1);
-            if (alarmId == -1) {
-                alarmId = 0;
-            } else {
-                alarmId += 8;
-                if (alarmId >= Integer.MAX_VALUE) {
-                    alarmId = 0;
-                }
-            }
-            prefs.edit().putInt("alarmId", alarmId).commit();
-
+        if (prefs == null) {
+            prefs = context.getSharedPreferences(MN.alarm.SHARED_PREFS_FILE, Context.MODE_PRIVATE);
         }
+
+        int alarmId = prefs.getInt("alarmId", -1);
+        if (alarmId == -1) {
+            alarmId = 0;
+        } else {
+            alarmId += 8;
+            if (alarmId >= Integer.MAX_VALUE) {
+                alarmId = 0;
+            }
+        }
+        prefs.edit().putInt("alarmId", alarmId).commit();
         return alarmId;
     }
 }
