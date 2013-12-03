e229bc5abc781c84aa929174093c5ff441325695
Wooseong Kim <kwosu87@me.com>
2013-12-03 09:19:07 +0900

    알람설정 액티비티 추가

---
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/alarm/pref/MNAlarmPreferenceActivity.java
@@ -0,0 +1,18 @@
+package com.yooiistudios.morningkit.alarm.pref;
+
+import android.app.ListActivity;
+import android.view.View;
+
+/**
+ * Created by StevenKim in MorningKit from Yooii Studios Co., LTD. on 2013. 12. 3.
+ *
+ * MNAlarmPreferenceActivity
+ * 알람을 추가, 수정하는 액티비티
+ */
+public class MNAlarmPreferenceActivity extends ListActivity implements View.OnClickListener{
+    
+    @Override
+    public void onClick(View v) {
+
+    }
+}
