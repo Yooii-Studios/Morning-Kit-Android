60222dc2d3ced028d29ae4c7093ebc5598689ffc
Wooseong Kim <kwosu87@me.com>
2013-12-14 20:29:14 +0900

    getChildCount는 제대로 된 값을 얻지 못하기에 getCount로 변경함

--- MorningKit/src/test/java/com/yooiistudios/morningkit/alarm/listview/MNAlarmListViewTest.java
+++ MorningKit/src/test/java/com/yooiistudios/morningkit/alarm/listview/MNAlarmListViewTest.java
@@ -59,7 +59,7 @@ public class MNAlarmListViewTest {
     public void alarmItemsTest() {
         assertThat(mainActivity.getAlarmListView().getCount(), is(alarmListAdaptor.getCount()));
 
-        for(int i=0; i<mainActivity.getAlarmListView().getChildCount()-1; i++) {
+        for(int i=0; i<mainActivity.getAlarmListView().getCount()-1; i++) {
             MNAlarmItemScrollView alarmItemScrollView = (MNAlarmItemScrollView) mainActivity.getAlarmListView().getChildAt(i);
             assertThat(alarmItemScrollView, notNullValue());
             assertThat(alarmItemScrollView, is(MNAlarmItemScrollView.class));
