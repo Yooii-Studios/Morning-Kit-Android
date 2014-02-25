7374aa3cbbeef92b5a91c647ad6f9858a57f9e44
Wooseong Kim <kwosu87@me.com>
2014-02-25 15:23:46 +0900

    Revert "플리커 디테일 프래그먼트 scaleType centerInside 로 변경"
    This reverts commit 74c1c89c9ad6c406d0fb1a24c8b0e54fb9eb1150.

--- MorningKit/src/main/java/com/yooiistudios/morningkit/panel/flickr/detail/MNFlickrDetailFragment.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/panel/flickr/detail/MNFlickrDetailFragment.java
@@ -1,8 +1,6 @@
 package com.yooiistudios.morningkit.panel.flickr.detail;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Build;
@@ -14,7 +12,6 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.stevenkim.waterlily.bitmapfun.ui.RecyclingImageView;
 import com.stevenkim.waterlily.bitmapfun.util.RecyclingBitmapDrawable;
@@ -33,7 +30,6 @@ import butterknife.Optional;
 
 import static com.yooiistudios.morningkit.panel.flickr.MNFlickrPanelLayout.FLICKR_DATA_GRAYSCALE;
 import static com.yooiistudios.morningkit.panel.flickr.MNFlickrPanelLayout.FLICKR_DATA_KEYWORD;
-import static com.yooiistudios.morningkit.panel.flickr.MNFlickrPanelLayout.FLICKR_DATA_PHOTO_URL;
 import static com.yooiistudios.morningkit.panel.flickr.MNFlickrPanelLayout.FLICKR_PREFS;
 import static com.yooiistudios.morningkit.panel.flickr.MNFlickrPanelLayout.FLICKR_PREFS_KEYWORD;
 
@@ -42,15 +38,13 @@ import static com.yooiistudios.morningkit.panel.flickr.MNFlickrPanelLayout.FLICK
  *
  * MNFlickrDetailFragment
  */
-public class MNFlickrDetailFragment extends MNPanelDetailFragment implements MNFlickrBitmapSaveAsyncTask.MNFlickrBitmapSaveAsyncTaskListener {
+public class MNFlickrDetailFragment extends MNPanelDetailFragment {
 
     @InjectView(R.id.flickr_detail_imageview) RecyclingImageView imageView;
     @InjectView(R.id.flickr_detail_edittext) EditText keywordEditText;
     @InjectView(R.id.flickr_detail_grayscale_textview) TextView grayScaleTextView;
     @Optional @InjectView(R.id.flickr_detail_grayscale_checkbox) CheckBox grayscaleCheckbox; // < V14
     Switch grayscaleSwitch; // >= V14
-
-    boolean isGrayScale;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,7 +54,7 @@ public class MNFlickrDetailFragment extends MNPanelDetailFragment implements MNF
             ButterKnife.inject(this, rootView);
 
             // 버전 체크해 스위치 얻어내기
-            isGrayScale = false;
+            boolean isGrayScale = false;
             if (getPanelDataObject().has(FLICKR_DATA_GRAYSCALE)) {
                 try {
                     isGrayScale = getPanelDataObject().getBoolean(FLICKR_DATA_GRAYSCALE);
@@ -82,7 +76,6 @@ public class MNFlickrDetailFragment extends MNPanelDetailFragment implements MNF
                         ExternalStorageManager.APP_DIRECTORY_HIDDEN + "/flickr");
                 if (bitmap != null) {
                     imageView.setImageDrawable(new RecyclingBitmapDrawable(getResources(), bitmap));
-                    setImgViewOnClickListener();
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -90,15 +83,10 @@ public class MNFlickrDetailFragment extends MNPanelDetailFragment implements MNF
 
             // 키워드 텍스트
             try {
-                String keywordString = getPanelDataObject().getString(FLICKR_DATA_KEYWORD);
-                keywordEditText.setText(keywordString);
-                keywordEditText.setSelection(keywordString.length());
-                keywordEditText.setPrivateImeOptions("defaultInputmode=english;");
-                keywordEditText.requestFocus();
+                keywordEditText.setText(getPanelDataObject().getString(FLICKR_DATA_KEYWORD));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
         }
         return rootView;
     }
@@ -122,41 +110,5 @@ public class MNFlickrDetailFragment extends MNPanelDetailFragment implements MNF
             SharedPreferences prefs = getActivity().getSharedPreferences(FLICKR_PREFS, Context.MODE_PRIVATE);
             prefs.edit().putString(FLICKR_PREFS_KEYWORD, keywordEditText.getText().toString()).commit();
         }
-    }
-
-    private void setImgViewOnClickListener() {
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (getPanelDataObject().has(FLICKR_DATA_PHOTO_URL)) {
-                    // AlertDialog
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                    builder.setMessage(getResources().getString(R.string.flickr_save_to_library_guide))
-                            .setPositiveButton(getResources().getString(R.string.ok),
-                                    new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            try {
-                                                saveImageToLibrary(getPanelDataObject().getString(FLICKR_DATA_PHOTO_URL));
-                                            } catch (JSONException e) {
-                                                e.printStackTrace();
-                                            }
-                                        }
-                                    })
-                            .setNegativeButton(getResources().getString(R.string.cancel), null)
-                            .create().show();
-                }
-            }
-        });
-    }
-
-    private void saveImageToLibrary(String flickrUrlString) {
-        new MNFlickrBitmapSaveAsyncTask(flickrUrlString, isGrayScale, this, getActivity()).execute();
-    }
-
-    @Override
-    public void onBitmapSaveFinished() {
-        Toast.makeText(getActivity(), R.string.flickr_photo_saved, Toast.LENGTH_SHORT).show();
-        MNLog.now("onBitmapSaveFinished");
     }
 }
