7899790e89021d0bd74ee4d28311ba56fa55c81f
Wooseong Kim <kwosu87@me.com>
2014-02-27 15:25:46 +0900

    Revert "[Delivers #66563026] 플리커 디테일 패널 교체 후에 EditText 포커싱이 잡히지 않는 현상 해결"
    This reverts commit 2bd562368930dfe15c858791eec6c3e3a9f782fa.

--- MorningKit/src/main/java/com/yooiistudios/morningkit/panel/memo/MNMemoPanelLayout.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/panel/memo/MNMemoPanelLayout.java
@@ -2,8 +2,6 @@ package com.yooiistudios.morningkit.panel.memo;
 
 /**
  * Created by StevenKim in MorningKit from Yooii Studios Co., LTD. on 2014. 2. 26.
- *
- * MNMemoPanelLayout
  */
 public class MNMemoPanelLayout {
 }
