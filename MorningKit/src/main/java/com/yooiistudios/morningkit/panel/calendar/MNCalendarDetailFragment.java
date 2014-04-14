c4760b17321ad7ec426f3db53b9c2ca80f3493b4
Wooseong Kim <kwosu87@me.com>
2014-04-14 12:17:20 +0900

    캘린더 패널/디테일 파일 추

---
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/panel/calendar/MNCalendarDetailFragment.java
@@ -0,0 +1,7 @@
+package com.yooiistudios.morningkit.panel.calendar;
+
+/**
+ * Created by StevenKim in MorningKit from Yooii Studios Co., LTD. on 2014. 4. 14.
+ */
+public class MNCalendarDetailFragment {
+}
