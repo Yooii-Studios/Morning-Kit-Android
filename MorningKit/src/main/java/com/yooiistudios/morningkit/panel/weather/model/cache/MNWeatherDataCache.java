e766b11980a462a6b942d9268a0d41baf68341e2
Wooseong Kim <kwosu87@me.com>
2014-04-18 13:36:02 +0900

    [Delivers #69751242] 날씨 캐시부분 로직의 문제 해결하기

--- MorningKit/src/main/java/com/yooiistudios/morningkit/panel/weather/model/cache/MNWeatherDataCache.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/panel/weather/model/cache/MNWeatherDataCache.java
@@ -4,6 +4,8 @@ import android.content.Context;
 
 import com.yooiistudios.morningkit.panel.weather.model.locationinfo.MNWeatherData;
 
+import org.joda.time.DateTime;
+
 import java.util.List;
 
 /**
@@ -13,6 +15,7 @@ import java.util.List;
  *  날씨 데이터들을 캐싱하는 클래스
  */
 public abstract class MNWeatherDataCache {
+    private static final String TAG = "MNWeatherDataCache";
     private static final int CACHE_LIMIT = 10;
     protected  static final int WEATHER_REFRESH_LIMIT_DISTANCE = (35 * 1000);
     protected static final int WEATHER_REFRESH_LIMIT_TIME_IN_MILLIS = (60 * 60 * 4 * 1000); // second -> 4 hours;
@@ -24,6 +27,20 @@ public abstract class MNWeatherDataCache {
     public MNWeatherDataCache(Context context) {
         this.context = context;
         unarchiveWeatherDataCacheList(context);
+
+        // REFRESH_LIMIT 시간이 지난 데이터는 모두 삭제하고 다시 저장하기
+        int previousSize = weatherDataCacheList.size();
+        for (int i = 0; i < weatherDataCacheList.size(); i++) {
+            MNWeatherData weatherData = weatherDataCacheList.get(i);
+            if (DateTime.now().getMillis() - weatherData.timeStampInMillis >= WEATHER_REFRESH_LIMIT_TIME_IN_MILLIS) {
+                weatherDataCacheList.remove(i);
+            }
+        }
+
+        // 변경사항이 있다면 저장
+        if (previousSize != weatherDataCacheList.size()) {
+            archiveWeatherData(context);
+        }
     }
 
     protected abstract void unarchiveWeatherDataCacheList(Context context);
@@ -36,9 +53,10 @@ public abstract class MNWeatherDataCache {
             weatherDataCacheList.remove(0);
         }
         weatherDataCacheList.add(weatherData);
-
-        archiveWeatherData(weatherData, context);
+        archiveWeatherData(context);
     }
 
-    protected abstract void archiveWeatherData(MNWeatherData weatherData, Context context);
+    protected abstract void archiveWeatherData(Context context);
+
+    public List<MNWeatherData> getWeatherDataCacheList() { return weatherDataCacheList; }
 }
