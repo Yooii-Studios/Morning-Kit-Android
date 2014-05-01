f9faa59d7ea15ce2f32db1b093093563edd9bb73
Wooseong Kim <kwosu87@me.com>
2014-05-01 18:27:13 +0900

    테마 - 디테일 - 포토 쪽 그림자 삭

--- MorningKit/src/main/java/com/yooiistudios/morningkit/setting/theme/themedetail/photo/MNThemePhotoListAdapter.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/setting/theme/themedetail/photo/MNThemePhotoListAdapter.java
@@ -14,11 +14,10 @@ import android.widget.TextView;
 
 import com.stevenkim.photo.SKBitmapLoader;
 import com.yooiistudios.morningkit.R;
-import com.yooiistudios.morningkit.common.shadow.RoundShadowRelativeLayout;
-import com.yooiistudios.morningkit.common.shadow.factory.MNShadowLayoutFactory;
 import com.yooiistudios.morningkit.common.sound.MNSoundEffectsPlayer;
 import com.yooiistudios.morningkit.setting.theme.soundeffect.MNSound;
 import com.yooiistudios.morningkit.setting.theme.themedetail.MNSettingColors;
+import com.yooiistudios.morningkit.setting.theme.themedetail.MNSettingResources;
 import com.yooiistudios.morningkit.setting.theme.themedetail.MNTheme;
 import com.yooiistudios.morningkit.setting.theme.themedetail.MNThemeType;
 
@@ -88,41 +87,38 @@ public class MNThemePhotoListAdapter extends BaseAdapter {
 
             viewHolder.getOuterLayout().setBackgroundColor(MNSettingColors.getBackwardBackgroundColor(currentThemeType));
             viewHolder.getTextView().setTextColor(MNSettingColors.getMainFontColor(currentThemeType));
+            viewHolder.getInnerLayout().setBackgroundResource(MNSettingResources.getItemSelectorResourcesId(currentThemeType));
 
             // theme - shadow
-            RoundShadowRelativeLayout roundShadowRelativeLayout = (RoundShadowRelativeLayout) convertView.findViewById(viewHolder.getShadowLayout().getId());
+//            RoundShadowRelativeLayout roundShadowRelativeLayout = (RoundShadowRelativeLayout) convertView.findViewById(viewHolder.getShadowLayout().getId());
 
             // 기존 동적 생성에서 색상 값만 바꾸어 주게 변경
 //            RoundShadowRelativeLayout newShadowRelativeLayout = MNShadowLayoutFactory.changeShadowLayout(currentThemeType, roundShadowRelativeLayout, viewHolder.getOuterLayout());
-            MNShadowLayoutFactory.changeThemeOfShadowLayout(roundShadowRelativeLayout, activity);
+//            MNShadowLayoutFactory.changeThemeOfShadowLayout(roundShadowRelativeLayout, activity);
 
             // onClick
-            if (roundShadowRelativeLayout != null) {
-                roundShadowRelativeLayout.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (MNSound.isSoundOn(activity)) {
-                            MNSoundEffectsPlayer.play(R.raw.effect_view_open, activity);
-                        }
-                        Intent intent = new Intent(Intent.ACTION_PICK);
-                        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-                        intent.putExtra("outputformat", Bitmap.CompressFormat.PNG.name());
-                        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                        // 사진 선택 기능으로 간다
-                        // 기존에 로딩한 사진이 있다면 recycle 반드시 필요
-                        if (position == 0) {
-                            // Portrait
-                            activity.startActivityForResult(intent, SKBitmapLoader.REQ_CODE_PICK_IMAGE_PORTRAIT);
-                        } else {
-                            // Landscape
-                            activity.startActivityForResult(intent, SKBitmapLoader.REQ_CODE_PICK_IMAGE_LANDSCAPE);
-                        }
+            viewHolder.getInnerLayout().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (MNSound.isSoundOn(activity)) {
+                        MNSoundEffectsPlayer.play(R.raw.effect_view_open, activity);
                     }
-                });
-            } else {
-                throw new AssertionError("shadowRelativeLayout must not be null!");
-            }
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                    intent.putExtra("outputformat", Bitmap.CompressFormat.PNG.name());
+                    intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                    // 사진 선택 기능으로 간다
+                    // 기존에 로딩한 사진이 있다면 recycle 반드시 필요
+                    if (position == 0) {
+                        // Portrait
+                        activity.startActivityForResult(intent, SKBitmapLoader.REQ_CODE_PICK_IMAGE_PORTRAIT);
+                    } else {
+                        // Landscape
+                        activity.startActivityForResult(intent, SKBitmapLoader.REQ_CODE_PICK_IMAGE_LANDSCAPE);
+                    }
+                }
+            });
         }
         return convertView;
     }
@@ -132,10 +128,7 @@ public class MNThemePhotoListAdapter extends BaseAdapter {
      */
     static class MNThemePhotoItemViewHolder {
         @Getter @InjectView(R.id.setting_theme_detail_photo_item_outer_layout)      RelativeLayout outerLayout;
-        @Getter @InjectView(R.id.setting_theme_detail_photo_item_shadow_layout)
-        RoundShadowRelativeLayout shadowLayout;
         @Getter @InjectView(R.id.setting_theme_detail_photo_item_inner_layout)      RelativeLayout innerLayout;
-
         @Getter @InjectView(R.id.setting_theme_detail_photo_item_crop_imageview)    ImageView cropImageView;
         @Getter @InjectView(R.id.setting_theme_detail_photo_item_divider_imageview) ImageView dividerImageView;
         @Getter @InjectView(R.id.setting_theme_detail_photo_item_textview)          TextView textView;
