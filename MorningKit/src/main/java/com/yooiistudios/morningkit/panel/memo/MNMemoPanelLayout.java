e7a27bed088e336d7a3a58c5dcafc177d03ec182
Wooseong Kim <kwosu87@me.com>
2014-05-27 14:39:03 +0900

    [Delivers #72057428 #72057432] 메모 디테일 리사이즈 연구하기 / 메모 메인 패널 구현

--- MorningKit/src/main/java/com/yooiistudios/morningkit/panel/memo/MNMemoPanelLayout.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/panel/memo/MNMemoPanelLayout.java
@@ -2,12 +2,16 @@ package com.yooiistudios.morningkit.panel.memo;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.SpannableStringBuilder;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.yooiistudios.morningkit.R;
+import com.yooiistudios.morningkit.common.dp.DipToPixel;
+import com.yooiistudios.morningkit.common.size.MNViewSizeMeasure;
+import com.yooiistudios.morningkit.common.textview.AutoResizeTextView;
 import com.yooiistudios.morningkit.panel.core.MNPanelLayout;
 import com.yooiistudios.morningkit.setting.theme.themedetail.MNTheme;
 import com.yooiistudios.morningkit.setting.theme.themedetail.MNThemeType;
@@ -27,7 +31,7 @@ public class MNMemoPanelLayout extends MNPanelLayout {
     public static final String MEMO_PREFS_CONTENT = "MEMO_PREFS_CONTENT";
     public static final String MEMO_DATA_CONTENT = "MEMO_DATA_CONTENT";
 
-    private TextView memoTextView;
+    private AutoResizeTextView memoTextView;
     private String memoString;
 
     public MNMemoPanelLayout(Context context) {
@@ -43,10 +47,10 @@ public class MNMemoPanelLayout extends MNPanelLayout {
         super.init();
 
         // TextView
-        memoTextView = new TextView(getContext());
+        memoTextView = new AutoResizeTextView(getContext());
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT);
-        int margin = (int) getResources().getDimension(R.dimen.margin_outer);
+        int margin = (int) getResources().getDimension(R.dimen.panel_quotes_padding);
         layoutParams.setMargins(margin, margin, margin, margin);
         memoTextView.setLayoutParams(layoutParams);
         getContentLayout().addView(memoTextView);
@@ -74,6 +78,11 @@ public class MNMemoPanelLayout extends MNPanelLayout {
         super.updateUI();
         if (memoString != null) {
             hideCoverLayout();
+            SpannableStringBuilder stringBuilder = new SpannableStringBuilder();
+            stringBuilder.append(memoString);
+            memoTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                    getResources().getDimensionPixelSize(R.dimen.panel_exchange_rates_main_font_size));
+            memoTextView.setMinTextSize(DipToPixel.dpToPixel(getContext(), 1));
             memoTextView.setText(memoString);
         } else {
             // 추후 패널쪽으로 올려서 전체적으로 구현할 예정: 패널이름(중앙) + 설명(아래쪽)
@@ -88,5 +97,23 @@ public class MNMemoPanelLayout extends MNPanelLayout {
         MNThemeType currentThemeType = MNTheme.getCurrentThemeType(getContext().getApplicationContext());
         memoTextView.setTextColor(
                 MNMainColors.getQuoteContentTextColor(currentThemeType, getContext().getApplicationContext()));
+    }
+
+    /**
+     * Rotate
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        // 로딩이 끝나기 전에는 진행하지 않음
+        if (memoString != null) {
+            MNViewSizeMeasure.setViewSizeObserver(this, new MNViewSizeMeasure.OnGlobalLayoutObserver() {
+                @Override
+                public void onLayoutLoad() {
+                    updateUI();
+                }
+            });
+
+        }
     }
 }
