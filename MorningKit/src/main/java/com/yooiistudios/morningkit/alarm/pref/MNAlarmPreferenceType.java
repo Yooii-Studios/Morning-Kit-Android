26ff73736beb6bde6a49ef97b506d53a5205cb63
Wooseong Kim <kwosu87@me.com>
2014-06-11 15:04:49 +0900

    사소한 코드 수정

--- MorningKit/src/main/java/com/yooiistudios/morningkit/alarm/pref/MNAlarmPreferenceType.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/alarm/pref/MNAlarmPreferenceType.java
@@ -4,4 +4,4 @@ package com.yooiistudios.morningkit.alarm.pref;
  * Created by StevenKim in MorningKit from Yooii Studios Co., LTD. on 2013. 12. 5.
  * MNAlarmPreferenceType
  */
-public enum MNAlarmPreferenceType { ADD, EDIT; }
+public enum MNAlarmPreferenceType { ADD, EDIT }
