9c4c1038f9d5a0fe92315a22639ee07039faaff5
Wooseong Kim <kwosu87@me.com>
2014-06-11 18:18:09 +0900

    불필요한 import 삭제

--- libraries/SKAlarmSound/src/main/java/com/yooiistudios/stevenkim/alarmsound/SKAlarmSoundDialog.java
+++ libraries/SKAlarmSound/src/main/java/com/yooiistudios/stevenkim/alarmsound/SKAlarmSoundDialog.java
@@ -9,7 +9,6 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.widget.ListView;
 
 import java.io.IOException;
