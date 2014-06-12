b1e96e86a476fc91973c3d4381ab63313e3b2477
Wooseong Kim <kwosu87@me.com>
2014-06-12 12:32:45 +0900

    네이버 앱스토어를 사용하면 리뷰를 네이버 앱스토어에 가서 남기게 구현. 조금 더 만질 필요 있음.

--- MorningKit/src/main/java/com/yooiistudios/morningkit/common/review/MNReviewApp.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/common/review/MNReviewApp.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.widget.Toast;
 
+import com.yooiistudios.morningkit.setting.store.MNStoreFragment;
+
 /**
  * Created by StevenKim in Morning Kit from Yooii Studios Co., LTD. on 2014. 1. 28.
  *
@@ -16,7 +18,12 @@ public class MNReviewApp {
     public static int REQ_REVIEW_APP = 4444;
 
     public static void showReviewActivity(Context context) {
-        Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
+        Uri uri;
+        if (MNStoreFragment.IS_STORE_FOR_NAVER) {
+            uri = (Uri.parse("http://nstore.naver.com/appstore/web/detail.nhn?productNo=1500436#"));
+        } else {
+            uri = Uri.parse("market://details?id=" + context.getPackageName());
+        }
         Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
         try {
             ((Activity)context).startActivityForResult(goToMarket, REQ_REVIEW_APP);
