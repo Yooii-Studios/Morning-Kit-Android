50685bb203803c63f1bea4b4cbd47568168bb0d4
Wooseong Kim <kwosu87@me.com>
2014-06-18 12:02:43 +0900

    DG사에서 알려준 대로 모닝키트 id를 수정, 하지만 광고는 아직 뜨지 않음.

--- MorningKit/src/main/java/com/yooiistudios/morningkit/common/ad/MNAdUtils.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/common/ad/MNAdUtils.java
@@ -73,7 +73,9 @@ public class MNAdUtils {
                         OneSDK sdk = OneSDK.getInstance(activity);
 
                         // Open service
-                        dgService = sdk.OpenService(4820, 8, 2, Constants.ServiceCategories.INTERSTITIAL, activity);
+                        // 4820 = publisher ID = Yooii Studios
+                        // 19 = App ID = Morning Kit
+                        dgService = sdk.OpenService(4820, 19, 2, Constants.ServiceCategories.INTERSTITIAL, activity);
 
                         // 중앙 계산
                         int x = (MNDeviceSizeInfo.getDeviceWidth(activity) - DipToPixel.getPixel(activity, 320)) / 2;
