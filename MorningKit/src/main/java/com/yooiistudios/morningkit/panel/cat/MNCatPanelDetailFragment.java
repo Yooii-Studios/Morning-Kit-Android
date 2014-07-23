a8c8c3ff87e40c317926e32613588174c882be39
Wooseong Kim <kwosu87@me.com>
2014-07-23 16:13:31 +0900

    텅 빈 디테일용 xml을 추가.

--- MorningKit/src/main/java/com/yooiistudios/morningkit/panel/cat/MNCatPanelDetailFragment.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/panel/cat/MNCatPanelDetailFragment.java
@@ -1,8 +1,16 @@
 package com.yooiistudios.morningkit.panel.cat;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yooiistudios.morningkit.R;
 import com.yooiistudios.morningkit.panel.core.detail.MNPanelDetailFragment;
 
 import org.json.JSONException;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Wooseong Kim in MorningKit from Yooii Studios Co., LTD. on 2014. 7. 23.
@@ -10,6 +18,16 @@ import org.json.JSONException;
  * MNCatPanelDetailFragment
  */
 public class MNCatPanelDetailFragment extends MNPanelDetailFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.panel_cat_detail_fragment, container, false);
+        if (rootView != null) {
+            ButterKnife.inject(this, rootView);
+        }
+        return rootView;
+    }
+
     @Override
     protected void archivePanelData() throws JSONException {
 
