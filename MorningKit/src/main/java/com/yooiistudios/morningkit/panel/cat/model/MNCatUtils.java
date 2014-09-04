d1f794980808d5a81f735e9297a09382f76657f8
Wooseong Kim <kwosu87@me.com>
2014-09-05 01:09:07 +0900

    고양이 애니메이션 2종 추가

--- MorningKit/src/main/java/com/yooiistudios/morningkit/panel/cat/model/MNCatUtils.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/panel/cat/model/MNCatUtils.java
@@ -1,7 +1,5 @@
 package com.yooiistudios.morningkit.panel.cat.model;
 
-import android.content.Context;
-
 import com.yooiistudios.morningkit.R;
 
 import org.joda.time.DateTime;
@@ -15,8 +13,8 @@ import java.util.Random;
  *  시간대에 맞게 고양이 애니메이션 리소스 id를 제공함
  */
 public class MNCatUtils {
-    private static final int NUM_OF_MORNING_ANIM = 2;
-    private static final int NUM_OF_NOON_ANIM = 2;
+    private static final int NUM_OF_MORNING_ANIM = 3;
+    private static final int NUM_OF_NOON_ANIM = 3;
     private static final int NUM_OF_EVENING_ANIM = 2;
     private static final int NUM_OF_NIGHT_ANIM = 2;
 
@@ -41,6 +39,8 @@ public class MNCatUtils {
                     return R.drawable.cat_animation_morning_set_1;
                 case 1:
                     return R.drawable.cat_animation_morning_set_2;
+                case 2:
+                    return R.drawable.cat_animation_morning_set_3;
                 default:
                     return R.drawable.cat_animation_morning_set_1;
             }
@@ -52,6 +52,8 @@ public class MNCatUtils {
                     return R.drawable.cat_animation_noon_set_1;
                 case 1:
                     return R.drawable.cat_animation_noon_set_2;
+                case 2:
+                    return R.drawable.cat_animation_noon_set_3;
                 default:
                     return R.drawable.cat_animation_noon_set_1;
             }
