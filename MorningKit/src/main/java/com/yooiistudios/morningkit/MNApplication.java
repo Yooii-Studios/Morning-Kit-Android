0abd9827ab0cea670bb6260f6d5510e0fa61b1f9
Wooseong Kim <kwosu87@me.com>
2014-12-01 17:13:32 +0900

    MNApplication에 필요한 정보 추가

--- MorningKit/src/main/java/com/yooiistudios/morningkit/MNApplication.java
+++ MorningKit/src/main/java/com/yooiistudios/morningkit/MNApplication.java
@@ -3,9 +3,12 @@ package com.yooiistudios.morningkit;
 import android.app.Application;
 import android.content.res.Configuration;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.yooiistudios.morningkit.setting.theme.language.MNLanguage;
 import com.yooiistudios.morningkit.setting.theme.language.MNLanguageType;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 /**
@@ -16,6 +19,35 @@ import java.util.Locale;
  */
 public class MNApplication extends Application {
     private Locale locale = null;
+    private static final String PROPERTY_ID = "UA-50855812-20";
+
+    /**
+     * Enum used to identify the tracker that needs to be used for tracking.
+     *
+     * A single tracker is usually enough for most purposes. In case you do need multiple trackers,
+     * storing them all in Application object helps ensure that they are created only once per
+     * application instance.
+     */
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this app.
+        GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg: roll-up tracking.
+        ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from a company.
+    }
+
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+
+    synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!mTrackers.containsKey(trackerId)) {
+
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            Tracker t = (trackerId == TrackerName.APP_TRACKER) ? analytics.newTracker(PROPERTY_ID)
+                    : (trackerId == TrackerName.GLOBAL_TRACKER) ? analytics.newTracker(R.xml.global_tracker)
+                    : analytics.newTracker(R.xml.ecommerce_tracker);
+            mTrackers.put(trackerId, t);
+
+        }
+        return mTrackers.get(trackerId);
+    }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
