ce50e1921755e54e845dbfaea72711fd627aff86
Wooseong Kim <kwosu87@me.com>
2015-10-21 19:45:15 +0900

    음악 선택 쪽 로직 실수 수정

---
+++ libraries/SKAlarmSound/src/main/java/com/yooiistudios/stevenkim/alarmsound/RingtoneUtils.java
@@ -0,0 +1,40 @@
+package com.yooiistudios.stevenkim.alarmsound;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.Settings;
+
+import com.crashlytics.android.Crashlytics;
+
+/**
+ * Created by Wooseong Kim in MorningKit from Yooii Studios Co., LTD. on 2015. 10. 21.
+ *
+ * RingtoneUtils
+ *  시스템 벨소리 URI 를 Media URI 로 변화환시켜주는 유틸리티 클래스
+ **/
+public class RingtoneUtils {
+    private RingtoneUtils() { throw new AssertionError("You MUST not create class!"); }
+
+    public static Uri getSystemRingtoneMediaUri(Context context) {
+        Uri systemRingtoneUri = Settings.System.DEFAULT_RINGTONE_URI;
+        Uri mediaUri = systemRingtoneUri;
+
+        if (systemRingtoneUri.getAuthority().equals(Settings.AUTHORITY)) {
+            Cursor c = null;
+            try {
+                c = context.getContentResolver().query(systemRingtoneUri, new String[]{
+                        Settings.NameValueTable.VALUE}, null, null, null);
+                if (c != null && c.moveToFirst()) {
+                    String val = c.getString(0);
+                    mediaUri = Uri.parse(val);
+                }
+            } catch (Exception e) {
+                Crashlytics.getInstance().core.logException(e);
+            } finally {
+                c.close();
+            }
+        }
+        return mediaUri;
+    }
+}
