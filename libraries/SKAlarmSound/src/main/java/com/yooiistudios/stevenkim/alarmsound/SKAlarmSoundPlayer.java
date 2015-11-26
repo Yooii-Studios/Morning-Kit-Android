43165be875657a45dea1737e4770bb1fa5e09cb3
Wooseong Kim <kwosu87@me.com>
2015-11-26 16:54:12 +0900

    Revert "제대로 핸들링이 된 경우 report를 하지 않게 수정"
    This reverts commit a98aa24563cc048725ac46c20c498bf332254f2c.

--- libraries/SKAlarmSound/src/main/java/com/yooiistudios/stevenkim/alarmsound/SKAlarmSoundPlayer.java
+++ libraries/SKAlarmSound/src/main/java/com/yooiistudios/stevenkim/alarmsound/SKAlarmSoundPlayer.java
@@ -196,6 +196,7 @@ public class SKAlarmSoundPlayer {
                 }
             }
         } catch (Exception e) {
+            reportAlarmSoundExceptionToCrashlytics(alarmSound, e);
             try {
                 setDataSourceUsingFileDescriptor(getMediaPlayer(), fileInfo);
             } catch (Exception e1) {
